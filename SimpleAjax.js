601be2e4af2c70780bb955df2aed7c7db88d3580
Santosh Kshirsagar <santoshkshirsagar97@rediffmail.com>
2016-12-29 18:03:44 +0530

    Create SimpleAjax.js

---
+++ SimpleAjax.js
@@ -0,0 +1,26 @@
+function SimpleAjax(str,div_id)
+{
+var xmlhttp;
+if (str=="")
+  {
+  document.getElementById(div_id).innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById(div_id).innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET",str,true);
+xmlhttp.send();
+}
